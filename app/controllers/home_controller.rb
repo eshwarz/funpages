58b8d4c9ce3f3064cb3f4c873b92878002456e60
Eshwar Chandra <eshwarcc@gmail.com>
2012-10-11 16:07:04 +0530

    Added layouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 class HomeController < ApplicationController
 
+	layout 'two_column'
+
 	def index
 		@hello = 'Hello world!'
 		@fruits = ['apple', 'banana', 'custard', 'water melon']
+	end
+
+	def second
+		render :layout => 'application'
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Funpages::Application.routes.draw do
   
   root :to => 'home#index'
 
+  controller :home do
+		match 'home', :action => 'index', :as => 'home'
+  	match 'second', :action => 'second', :as => 'second'
+  end
+
 end
